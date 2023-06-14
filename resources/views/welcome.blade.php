539be768617a1e7ddd0aaf3996e2877bb9836c01
phprola <pedro.h28@aluno.ifsc.edu.br>
2023-06-14 15:19:39 -0300

    Alteração no nav do welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,9 +18,10 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="./">Início</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="about">Caps AD</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="products">Caps </a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="store">Cardápio</a></li>
                     </ul>
                 </div>
             </div>
