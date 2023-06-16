eb557082a80f0d1145140730c6d20e47b544dbee
phprola <pedro.h28@aluno.ifsc.edu.br>
2023-06-16 14:32:34 -0300

    usuario-route,model,controller,form e list

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('base.dashboard');
+})
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('usuario', UsuarioController::class);
+    Route::post('usuario/search', [UsuarioController::class, 'search'])->name(
+        'usuario.search'
+    );
+});
